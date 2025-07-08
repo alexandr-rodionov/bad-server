9bc40233a05db7b10c62f3d5ed62a192fa92b47e
Alexandr Rodionov <rodya3003@yandex.ru>
2025-07-08 16:52:01 +0300

    bugfix/mv check img to ctrl

--- backend/src/middlewares/file.ts
+++ backend/src/middlewares/file.ts
@@ -1,20 +1,19 @@
-import { Express, Request } from 'express'
-import multer, { FileFilterCallback } from 'multer'
+import { Express, NextFunction, Request, Response } from 'express'
+import multer, { diskStorage, FileFilterCallback } from 'multer'
 import { extname, join } from 'path'
-import sharp from 'sharp'
 import fs from 'fs'
 import BadRequestError from '../errors/bad-request-error'
 
 const createDirectoryIfNotExist = (directory: string) => {
     if (!fs.existsSync(directory)) {
-      fs.mkdirSync(directory, { recursive: true });
+      fs.mkdirSync(directory, { recursive: true })
     }
-};
+}
 
 type DestinationCallback = (error: Error | null, destination: string) => void
 type FileNameCallback = (error: Error | null, filename: string) => void
 
-const storage = multer.diskStorage({
+const storage = diskStorage({
     destination: (
         _req: Request,
         _file: Express.Multer.File,
@@ -50,42 +49,18 @@ const types = [
     'image/svg+xml',
 ]
 
-const fileSizeLimits = {
-    min: 2 * 1024,
-    max: 10 * 1024 * 1024,
-}
-
-const imgMinRes = {
-    width: 50,
-    height: 50,
-}
-
 const fileFilter = (
     _req: Request,
     file: Express.Multer.File,
     cb: FileFilterCallback
 ) => {
     if (!types.includes(file.mimetype)) {
-        return cb(new BadRequestError(`Недопустимый тип файла. Допустимые типы: ${types.join(', ')}`))
+        const ext = types.map(type => type.split('/')[1]);
+
+        return cb(new BadRequestError(`Недопустимый тип файла. Допустимые типы: .${ext.join(', .')}`))
     }
 
-    if (file.size < fileSizeLimits.min) {
-        return cb(new BadRequestError(`Размер файла слишком маленький. Минимальный размер: 2 KB`))
-    }
-
-    sharp(file.buffer)
-        .metadata()
-        .then((metadata) => {
-            if (metadata.width != null && metadata.height != null &&
-               (metadata.width < imgMinRes.width || metadata.height < imgMinRes.height)) {
-                return cb(new BadRequestError(`Минимальное разрешение изображения: ${imgMinRes.width}x${imgMinRes.height}px`))
-            }
-
-            return cb(null, true);
-        })
-        .catch(() => cb(new BadRequestError('Ошибка анализа изображения')))
+    return cb(null, true);
 }
 
-const limits = { fileSize: fileSizeLimits.max }
-
-export default multer({ storage, fileFilter, limits })
+export default multer({ storage, fileFilter })
